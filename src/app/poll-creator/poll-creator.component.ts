d07e60d51051a42abe7a1968678542022f1ea450
Patrick Coral <patrick9999@hotmail.ch>
2022-11-10 22:17:51 +0100

    Added check for empty string in poll-creator

--- src/app/poll-creator/poll-creator.component.ts
+++ src/app/poll-creator/poll-creator.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Poll } from '../models/poll';
 import { ResultsService } from '../results.service';
-import { Router } from "@angular/router";
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-poll-creator',
@@ -11,33 +11,44 @@ import { Router } from "@angular/router";
 export class PollCreatorComponent implements OnInit {
 	answerAmount: number = 4;
 	answers: string[] = Array<string>(this.answerAmount);
-  poll: Poll = new Poll(new Map<string, number>);
+	poll: Poll = new Poll(new Map<string, number>());
 
 	decrement() {
 		if (this.answers.length > 2) {
 			this.answerAmount--;
-      this.answers.length--;
+			this.answers.length--;
 		}
 	}
 
 	increment() {
 		this.answerAmount++;
-    this.answers.length++;
+		this.answers.length++;
 	}
 
 	print() {
+		for (let index = 0; index < this.answers.length; index++) {
+			if (this.answers[index] === undefined) return;
+			console.log("Can't return empty elements");			
+		}
+		
+		
 		this.answers.forEach((val, i) => {
 			this.poll?.results.set(val, 0);
 		});
-    
-    this.resultsService.createDoc(this.poll).then((docRef) => this.router.navigate([`/${docRef.id}`]));
-  }
 
-  trackByFn(index: any) {
-    return index;
-  }
+		this.resultsService
+			.createDoc(this.poll)
+			.then((docRef) => this.router.navigate([`/${docRef.id}`]));
+	}
 
-	constructor(private resultsService: ResultsService, private router: Router) {}
+	trackByFn(index: any) {
+		return index;
+	}
+
+	constructor(
+		private resultsService: ResultsService,
+		private router: Router
+	) {}
 
 	ngOnInit(): void {}
 }
